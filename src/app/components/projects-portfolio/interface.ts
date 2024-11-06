196483a55b8e210486798d23b69ff914b3a274eb
Rusbell Ruiz <rusbell.ruiz.p@gmail.com>
2024-11-05 21:50:25 -0500

    Adicion de enlaces ancla para la barra de navegacion

---
+++ src/app/components/projects-portfolio/interface.ts
@@ -0,0 +1,7 @@
+export interface ProjectInterface {
+    img: String,
+    title: String,
+    description: String,
+    urlCode: String,
+    urlPage: String
+}
--- src/app/components/projects-portfolio/projects-portfolio.component.ts
+++ src/app/components/projects-portfolio/projects-portfolio.component.ts
@@ -1,14 +1,40 @@
-import { Component } from '@angular/core';
+import { Component, OnInit, Injectable } from '@angular/core';
+import { HttpClient, HttpClientModule } from '@angular/common/http';
 import { ProjectCardComponent } from "../../components/sub-components/project-card/project-card.component";
+import { Observable } from 'rxjs';
+import { ProjectInterface } from './interface';
 
+@Injectable({
+  providedIn: 'root'
+})
 
 @Component({
   selector: 'app-projects-portfolio',
   standalone: true,
-  imports: [ProjectCardComponent],
+  imports: [ProjectCardComponent, HttpClientModule],
   templateUrl: './projects-portfolio.component.html',
-  styleUrl: './projects-portfolio.component.css'
+  styleUrls: ['./projects-portfolio.component.css']
 })
-export class ProjectsPortfolioComponent {
+export class ProjectsPortfolioComponent implements OnInit {
+  data: ProjectInterface[] = [];
+  path = "../../assets/projects.json";
 
+  constructor(private http: HttpClient) {}
+
+  ngOnInit() {
+    this.getJson_Service().subscribe((response: ProjectInterface[]) => {
+
+      if (Array.isArray(response)) {
+        this.data = response;
+      } else {
+        this.data = [response];
+      }
+    }, error => {
+      console.log("Error al obtener los datos del json");
+    });
+  }
+  
+  getJson_Service(): Observable<ProjectInterface[]> {
+    return this.http.get<ProjectInterface[]>(this.path);
+  }
 }
